70e3a3599c6bdf87ce65bd6ca040e1c4b8f1a78a
Yash Priyadarshan <yashpriyadarshan@gmail.com>
2026-04-28 18:01:39 +0530

    removing github pages and dploying on vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Breaking-Job-frontend/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
